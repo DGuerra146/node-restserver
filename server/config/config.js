1969896928944c5b2562d372e368f4a134d9d54e
Diego <dguerra.orellana@gmail.com>
2018-05-23 20:14:24 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 //PUERTO
 //=============
 process.env.PORT=process.env.PORT || 3000;
+
+
+//=============
+//ENTORNO
+//=============
+process.env.NODE_ENV=process.env.NODE_ENV || 'dev';
+
+
+//=============
+//BD
+//=============
+let urlDB;
+
+if(process.env.NODE_ENV==='dev'){
+	urlDB='mongodb://localhost:27017/cafe';
+}else{
+	urlDB='mongodb://cafe-user:1234567@ds133570.mlab.com:33570/cafe';
+}
+
+process.env.URLDB=urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config.js')
 const express = require('express');
+const mongoose=require('mongoose');
+
 const app = express();
 const bodyParser=require('body-parser');
 
@@ -9,39 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
- 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuarios')
-})
+ app.use(require('./routes/usuario.js'));
 
-app.post('/usuario', function (req, res) {
 
-	let body=req.body;
+//CONEXION
+mongoose.connect(process.env.URLDB,(err,res)=>{
+	if(err)throw err;
+	console.log('Base de datos ONLINE');
+});
 
-	if(body.nombre===undefined){
-		res.status(400).json({
-			ok:false,
-			mensaje:'El nombre es necesario'
-		})
-	}else{
- 	 res.json({
-  		persona:body
-  	})
-		
-	}
 
-})
 
-app.put('/usuario/:id', function (req, res) {
-	let id=req.params.id;
-  res.json({
-  	id
-  })
-})
- 
-app.delete('/usuario', function (req, res) {
-  res.json('delete Usuarios')
-})
+
+
 
 app.listen(process.env.PORT, ()=>{
 	console.log('Escuchando puerto:',process.env.PORT);
